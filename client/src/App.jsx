146f1a5b04efb7bd86520cf1699a95263c7c2b59
Ogunbiyi Temitope <ogunbiyitemitopeayodele@gmail.com>
2023-01-01 21:02:59 +0100

    More frontend changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar } from './components'
+import { Home } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className="relative sm:flex hidden mr-10">
+        <Sidebar/>
+        </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar/>
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      
+      </div>
+    </div>
   )
 }
 
